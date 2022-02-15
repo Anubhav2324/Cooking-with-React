a42a280d5ce61028d92accedff122c85021564d2
Anubhav2324 <kumaranubhav8844@gmail.com>
2022-02-15 23:02:36 +0530

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import Counter from "./Counter";
+import CounterHooks from "./CounterHooks";
+
+export const ThemeContext = React.createContext()
+
+function App() {
+  console.log('App')
+  const [theme, setTheme] = useState('red')
+  return(
+    <ThemeContext.Provider value={{background: theme}}>
+    Counter
+    <Counter initialCount = {0} />
+    CounterHooks
+    <CounterHooks initialCount = {0} />
+    <button onClick={()=> setTheme(prevTheme => {
+      return prevTheme === 'red' ? 'blue' : 'red'
+    })}>Toggle Context</button>
+    </ThemeContext.Provider>
+  )
+}
+
+export default App;
